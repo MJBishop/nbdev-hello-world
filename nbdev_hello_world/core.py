6fb578670ae21dc24484856a23cf56d44ce260fe
Mike Bishop <mike@drinklist.co>
2024-09-26 04:26:35 +0100

    tidy files

--- nbdev_hello_world/core.py
+++ nbdev_hello_world/core.py
@@ -16,33 +16,34 @@ from nbformat.v4 import new_notebook, new_markdown_cell, new_code_cell
 # %% ../nbs/01_core.ipynb 3
 from configparser import ConfigParser 
 
+# %% ../nbs/01_core.ipynb 4
 def repo_string():
     configure = ConfigParser() 
     configure.read('settings.ini') 
     lib_path = configure.get('DEFAULT','lib_path')
     return lib_path
 
-# %% ../nbs/01_core.ipynb 4
+# %% ../nbs/01_core.ipynb 5
 def filename_for_test_module(module_name, index_str):
     return f"{index_str}_test_{module_name}.ipynb"
 
-# %% ../nbs/01_core.ipynb 5
+# %% ../nbs/01_core.ipynb 6
 def test_module(module_name):
     return f'tests/test_{module_name}'
 
-# %% ../nbs/01_core.ipynb 6
+# %% ../nbs/01_core.ipynb 7
 def default_exp_string_for_test_module(module_name):
     return f'#| default_exp {test_module(module_name)}'
 
-# %% ../nbs/01_core.ipynb 7
+# %% ../nbs/01_core.ipynb 8
 def import_module_string_for_test_module(repo_name, module_name):
     return f'from {repo_name}.{module_name} import *'
 
-# %% ../nbs/01_core.ipynb 8
+# %% ../nbs/01_core.ipynb 9
 def nbdev_export_to_string():
     return "#| hide \nimport nbdev; nbdev.nbdev_export()"
 
-# %% ../nbs/01_core.ipynb 9
+# %% ../nbs/01_core.ipynb 10
 def format_test_module(module_name, index_str):
     fname = filename_for_test_module(module_name, index_str)
     repo_name = repo_string()
@@ -58,7 +59,7 @@ def format_test_module(module_name, index_str):
     cell2 = new_code_cell(cell2_string)
     nb['cells'].append(cell2)
 
-    cell3 = new_code_cell(nbdev_export_string())
+    cell3 = new_code_cell(nbdev_export_to_string())
     nb['cells'].append(cell3)
 
     # save the notebook to a file
@@ -67,19 +68,19 @@ def format_test_module(module_name, index_str):
         nbformat.write(nb, f)
         
 
-# %% ../nbs/01_core.ipynb 11
+# %% ../nbs/01_core.ipynb 12
 def filename_for_module(module_name, index_str):
     return f"{index_str}_{module_name}.ipynb"
 
-# %% ../nbs/01_core.ipynb 12
+# %% ../nbs/01_core.ipynb 13
 def default_exp_string_for_module(module_name):
     return f'#| default_exp {module_name}'
 
-# %% ../nbs/01_core.ipynb 13
+# %% ../nbs/01_core.ipynb 14
 def default_test_class_string_for_module(module_name):
     return f'Test{module_name.title()}'
 
-# %% ../nbs/01_core.ipynb 14
+# %% ../nbs/01_core.ipynb 15
 def format_module(module_name, index_str):
     fname = filename_for_module(module_name, index_str)
 
@@ -113,7 +114,7 @@ def format_module(module_name, index_str):
     nb['cells'].append(cell4)
 
     
-    cell5 = new_code_cell(nbdev_export_string())
+    cell5 = new_code_cell(nbdev_export_to_string())
     nb['cells'].append(cell5)
 
     # save the notebook to a file
